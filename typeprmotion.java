070c907109b72e016971f3d92fbf2e3e003b1229
VAIBHAV SINGHAL <125060733+vasinghal128@users.noreply.github.com>
2023-06-29 12:22:25 +0530

    java

---
+++ typeprmotion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class typeprmotion {
+    public static void main(String args[]){
+        char a = 'a';
+        char b = 'b';
+        System.out.println((int)(a));
+        System.out.println((int)(b));
+        System.out.print(b-a);
+    }    
+}
---
+++ uo.java
@@ -0,0 +1,20 @@
+public class uo {
+    public static void main(String args[]){
+        int a = 10;
+        int b = ++a;
+        int c = 10;
+        int d = c++;
+        int e = 10;
+        int f = --e;
+        int g = 10;
+        int h = g--;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+     }
+}
