d33ee6dcc94f2f0bc4a2cfb5614b34f4b167d9d2
VAIBHAV SINGHAL <125060733+vasinghal128@users.noreply.github.com>
2023-06-29 12:35:26 +0530

    java

---
+++ relationaloprators.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class relationaloprators{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of A:- ");
+        int A = sc.nextInt();
+        System.out.print("Enter value of B:- ");
+        int B = sc.nextInt();
+        System.out.println(A==B);
+        System.out.println(A<B);
+        System.out.println(A>B);
+        System.out.println(A>=B);
+        System.out.println(A<=B);
+    }
+}
